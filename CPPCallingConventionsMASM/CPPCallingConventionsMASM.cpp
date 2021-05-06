344e24b31d09be4d5f1dfb1647a8ea51f07858e3
Ivan MSI <arigatory@gmail.com>
2021-05-06 16:59:21 +0300

    fin

--- CPPCallingConventionsMASM/CPPCallingConventionsMASM.cpp
+++ CPPCallingConventionsMASM/CPPCallingConventionsMASM.cpp
@@ -1,7 +1,16 @@
 
 #include <iostream>
 
+
+extern "C" void CalculateSum(int a, int b, int c, int* s1, int* s2, int* s3);
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a = 2, b = 3, c = 4;
+    int s1, s2, s3;
+    
+    CalculateSum(a, b, c, &s1, &s2, &s3);
+    printf("Input a:\t%4d\tb:\t%4d\tc:\t%4d\n", a, b, c);
+    printf("Input sum:\t%4d\tsquare:\t%4d\tcube:\t%4d\n", s1, s2, s3);
+
+    return 0;
 }
